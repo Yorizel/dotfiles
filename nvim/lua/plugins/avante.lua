371f48eef41fb46eec1e16cc2bcd7dd6e3a85c6f
Manoel Carvalho <manoel.netocarvalho03@gmail.com>
2025-03-22 17:15:23 -0300

    feat(plugins): add Avante plugin mappings

---
+++ nvim/lua/plugins/avante.lua
@@ -0,0 +1,106 @@
+local prefix = "<Leader>A"
+return {
+  {
+    "yetone/avante.nvim",
+    build = vim.fn.has "win32" == 1 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        or "make",
+    event = "User AstroFile", -- load on file open because Avante manages it's own bindings
+    cmd = {
+      "AvanteAsk",
+      "AvanteBuild",
+      "AvanteEdit",
+      "AvanteRefresh",
+      "AvanteSwitchProvider",
+      "AvanteChat",
+      "AvanteToggle",
+      "AvanteClear",
+    },
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      { "AstroNvim/astrocore", opts = function(_, opts) opts.mappings.n[prefix] = { desc = " Avante" } end },
+    },
+    opts = {
+      -- other config
+      -- The system_prompt type supports both a string and a function that returns a string. Using a function here allows dynamically updating the prompt with mcphub
+      system_prompt = function()
+        local hub = require("mcphub").get_hub_instance()
+        return hub:get_active_servers_prompt()
+      end,
+      -- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+      custom_tools = function()
+        return {
+          require("mcphub.extensions.avante").mcp_tool(),
+        }
+      end,
+
+      mappings = {
+        ask = prefix .. "<CR>",
+        edit = prefix .. "e",
+        refresh = prefix .. "r",
+        focus = prefix .. "f",
+        select_model = prefix .. "?",
+        stop = prefix .. "S",
+        select_history = prefix .. "h",
+        toggle = {
+          default = prefix .. "t",
+          debug = prefix .. "d",
+          hint = prefix .. "h",
+          suggestion = prefix .. "s",
+          repomap = prefix .. "R",
+        },
+        diff = {
+          next = "]c",
+          prev = "[c",
+        },
+        files = {
+          add_current = prefix .. ".",
+        },
+      },
+    },
+
+    specs = { -- configure optional plugins
+      { "AstroNvim/astroui", opts = { icons = { Avante = "" } } },
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      { -- if copilot.lua is available, default to copilot provider
+        "zbirenbaum/copilot.lua",
+        specs = {
+          {
+            "yetone/avante.nvim",
+            opts = {
+              provider = "copilot",
+              auto_suggestions_provider = "copilot",
+            },
+          },
+        },
+      },
+
+      {
+        -- make sure `Avante` is added as a filetype
+        "OXY2DEV/markview.nvim",
+        optional = true,
+        opts = function(_, opts)
+          if not opts.filetypes then opts.filetypes = { "markdown", "quarto", "rmd" } end
+          opts.filetypes = require("astrocore").list_insert_unique(opts.filetypes, { "Avante" })
+        end,
+      },
+    },
+  },
+}

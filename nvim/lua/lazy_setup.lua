fbaff00ec967c3046161ca42476acd4f3508b20f
Manoel Carvalho <manoel.netocarvalho03@gmail.com>
2025-06-04 23:36:41 -0300

    chore: Update nvim packages and configurations

--- nvim/lua/lazy_setup.lua
+++ nvim/lua/lazy_setup.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
   },
   { import = "community" },
   { import = "plugins" },
+  { import = "lsp-servers" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
   install = { colorscheme = { "astrotheme", "habamax" } },

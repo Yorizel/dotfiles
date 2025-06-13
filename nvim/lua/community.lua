1f4dcc5891e2e91e73dec71c646a11828dffbf4a
Manoel Carvalho <manoel.netocarvalho03@gmail.com>
2025-06-13 20:01:27 -0300

    chore: update plugin lazy-lock

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -1,7 +1,8 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.file-explorer.mini-files" },
+  { import = "astrocommunity.file-explorer.oil-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.lsp.garbage-day-nvim" },
   { import = "astrocommunity.lsp.ts-error-translator-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
@@ -18,6 +19,7 @@ return {
   { import = "astrocommunity.motion.mini-surround" },
   { import = "astrocommunity.note-taking.obsidian-nvim" },
   { import = "astrocommunity.media.cord-nvim" },
+  { import = "astrocommunity.media.codesnap-nvim" },
   { import = "astrocommunity.markdown-and-latex.markview-nvim" },
   { import = "astrocommunity.git.blame-nvim" },
   { import = "astrocommunity.colorscheme.catppuccin" },
--- nvim/lua/lsp-servers/zig.lua
+++ nvim/lua/lsp-servers/zig.lua
@@ -1,5 +1,20 @@
 return {
   {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "astro" })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "zls" })
+    end,
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     optional = true,
     opts = function(_, opts)
